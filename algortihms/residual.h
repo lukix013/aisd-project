dd1c1e9ff74c12fa89163c2480766ee271260720
lukix013 <70850792+lukix013@users.noreply.github.com>
2025-04-29 10:48:56 +0200

    Add files via upload

---
+++ algortihms/residual.h
@@ -0,0 +1,30 @@
+#ifndef RESIDUAL_H
+#define RESIDUAL_H
+
+#include <vector>
+#include <limits>
+#include <queue>
+
+class ResidualNetwork {
+public:
+    struct Edge {
+        int from, to;
+        float capacity, flow;
+        Edge* reverse;
+
+        Edge(int from, int to, float capacity)
+            : from(from), to(to), capacity(capacity), flow(0), reverse(nullptr) {}
+    };
+
+    ResidualNetwork(int vertices);
+    void addEdge(int from, int to, float capacity);
+    float maxFlow(int source, int sink);
+
+private:
+    int vertices;
+    std::vector<std::vector<Edge*>> adjList;
+
+    bool bfs(int source, int sink, std::vector<Edge*>& parent);
+};
+
+#endif // RESIDUAL_H
