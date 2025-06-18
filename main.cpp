61140edc139f3731a86a4ae70e80c0aab0709929
lukix013 <70850792+lukix013@users.noreply.github.com>
2025-06-18 18:43:39 +0200

    aktualizacja main.cpp
    naprawiono drobne błędy

--- main.cpp
+++ main.cpp
@@ -18,17 +18,16 @@ int main() {
     int quartersCount;
     fin >> quartersCount;
 
-    std::vector<int> quartersValues(quartersCount);
+    std::vector<int> quartersValues(quartersCount); //Wartości plonów w poszczególnych ćwiartkach.
 
     for (int i = 0; i < quartersCount; i++) {
         fin >> quartersValues[i];
     }
 
-    std::vector<std::vector<Point>> quarters(quartersCount);
+    std::vector<std::vector<Point>> quarters(quartersCount); // Współrzędne granic ćwiartek.
 
     for (int i = 0; i < quartersCount; i++) {
         int pointsCount;
-
         fin >> pointsCount;
         quarters[i].resize(pointsCount);
 
@@ -37,6 +36,7 @@ int main() {
         }
     }
 
+    // Obliczanie otoczki wypukłej dla każdej ćwiartki
     for (auto &quarter : quarters) {
         convexHull(quarter);
     }
@@ -45,11 +45,10 @@ int main() {
     fin >> fieldCount;
     fin >> browCount;
 
-
-    int V1;
+    int V1; // Liczba wierzchołków w pierwszej sieci
     fin >> V1;
 
-    ResidualNetwork net1(V1);
+    ResidualNetwork net1(V1); //siec 1 dla pierwszej fazy
 
     for (int i = 0; i < fieldCount; i++) {
         Point p;
@@ -60,15 +59,19 @@ int main() {
     int source1, sink1;
     fin >> source1 >> sink1;
 
-    std::vector<int> poleProd(fieldCount, 0);
+    std::vector<int> poleProd(fieldCount, 0); //Produkcja jęczmienia na poszczególnych polach.
+
+    // Wyznaczanie produkcji jęczmienia na podstawie lokalizacji pól względem ćwiartek
     for (int i = 0; i < fieldCount; i++) {
         for (int j = 0; j < quartersCount; j++) {
             if (punkt_w_wielokacie(net1.coordinates[i], quarters[j])) {
                 poleProd[i] += quartersValues[j];
+                std::cout << quartersValues[j] << std::endl;
             }
         }
     }
 
+    // Dodanie krawędzi z pól do źródła w grafie
     for (int i = 0; i < fieldCount; i++) {
         net1.addEdge(source1, i, poleProd[i], 0);
     }
@@ -76,6 +79,7 @@ int main() {
     int edgesCount1;
     fin >> edgesCount1;
 
+    // Dodanie krawędzi między polami a browarami
     for (int i = 0; i < edgesCount1; i++) {
         int u, v;
         int capacity;
@@ -84,18 +88,28 @@ int main() {
         net1.addEdge(u, v, capacity, cost);
     }
 
+    // Obliczenie maksymalnego przepływu o minimalnym koszcie
     auto [flow1, cost1] = net1.minCostMaxFlow(source1, sink1);
+
     output << "Wasza Dostojność Samwise, \nPo długim i skrupulatnym badaniu pól, browaróœ, karczm oraz dróg Shire, zespół techniczny wspomagany komputerem przygotował plan zapewnienia dostatku piwa w każdej karczmie. \nEtap pierwszy\nPola iprawne zostały zbadane przez kartografóœ i botaników. Okazało się, że w zależności od ćwiartki kraju, w której leży dane pole, plony różnią się znacząco.\nNastępnie uruchomiliśmy transporty jęczmienia do browaróœ. Drogi były trudne, część z nich wymagała kosztownych napraw, ale nasze wozy zdołały dotrzeć do celu.\nUdało się przetransportować: " << flow1 << ", łączny koszt naprawy dróg wyniósł:" << cost1 << "\n";
 
     output << "W poszczególnych browarach znalazło się: \n";
-    std::vector<int> jeczmienBrowary(4, 0);
-    for (int i = V1 - browCount - 3; i < V1 - 3; i++) {
+
+    std::vector<int> jeczmienBrowary(browCount, 0); //Ilość jęczmienia w poszczególnych browarach.
+
+
+    std::cout << browCount << std::endl;
+
+    int browStartIndex = V1 - browCount - 3;
+
+    // Odczyt przepływu jęczmienia do browarów
+    for (int i = browStartIndex; i < browStartIndex + browCount + 1; i++) {
+
         for (Edge* e : net1.adjList[i]) {
             if(e->to == sink1){
-                jeczmienBrowary[i - 8] = e->flow;
-                output << "Jeczmien w browarze " << (i - 7) << ": " << e->flow << "\n";
+                jeczmienBrowary[i - browStartIndex] = e->flow;
+                output << "Jeczmien w browarze " << (i - browStartIndex) << ": " << e->flow << "\n";
             }
-        
         }
     }
 
@@ -103,18 +117,20 @@ int main() {
 
     int V2;
     fin >> V2;
-    ResidualNetwork net2(V2);
+    ResidualNetwork net2(V2); //Sieć 2 dla fzy 2
 
     int source2, sink2;
     fin >> source2 >> sink2;
 
-    for (int i = 0; i < 4; i++) {
+    // Dodanie krawędzi z browarów do źródła w drugiej sieci
+    for (int i = 0; i < browCount; i++) {
         net2.addEdge(source2, 8 + i, jeczmienBrowary[i], 0);
     }
 
     int edgesCount2;
     fin >> edgesCount2;
 
+    // Dodanie krawędzi między browarami a karczmami
     for (int i = 0; i < edgesCount2; i++) {
         int u, v;
         int capacity;
@@ -122,12 +138,13 @@ int main() {
 
         fin >> u >> v >> capacity >> cost;
         net2.addEdge(u, v, capacity, cost);
-
     }
 
+    // Obliczenie przepływu z browarów do karczm
     auto [flow2, cost2] = net2.minCostMaxFlow(source2, sink2);
-    output << flow2 << ", koszt naprawy dróg wyniósł: " << cost2 << "\n";
 
+    // Zapis wyników drugiego etapu do pliku
+    output << flow2 << ", koszt naprawy dróg wyniósł: " << cost2 << "\n";
     output << "Burmistrzu Samwise, dzięki Waszej mądrości i gościnności każdy hobbit w Shire będzie mógł wznieść świeżego piwa.\nZ szacunkiem i kuflem zminego piwa,\nTechniczny Zespół Hobbitonu.";
 
     fin.close();
