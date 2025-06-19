4ecfafea6acee84e76acff5be3a0d87414a5ed10
lukix013 <70850792+lukix013@users.noreply.github.com>
2025-06-19 19:01:48 +0200

    dodanie obsługi ujemnych cykli + komentarze

--- residual.cpp
+++ residual.cpp
@@ -1,6 +1,8 @@
 #include "residual.h"
 #include <iostream>
 #include <algorithm>
+#include <limits>
+#include <queue>  // dodane, bo brakowało
 #include "prz_wiel.h"
 
 ResidualNetwork::ResidualNetwork(int vertices) : vertices(vertices) {
@@ -89,6 +91,7 @@ bool ResidualNetwork::bellmanFord(int source, int sink, std::vector<Edge*>& pare
     dist[source] = 0;
 
     for (int i = 0; i < vertices - 1; i++) { // przechodzimy przez liczba wierzcholkow - 1 chyba że brak kosztu
+        
         bool updated = false; // brak aktualizacji kosztu
         for (int u = 0; u < vertices; u++) {
             if (dist[u] == std::numeric_limits<int>::max()) continue;
@@ -107,16 +110,77 @@ bool ResidualNetwork::bellmanFord(int source, int sink, std::vector<Edge*>& pare
     return parent[sink] != nullptr; // jeśli znaleziono pętle zwróć true
 }
 
+bool ResidualNetwork::findNegativeCycle(std::vector<Edge*> &cycle){
+    std::vector<int> dist(vertices, 0);
+    std::vector<Edge*> parent(vertices, nullptr);
+
+    int x = -1;
+
+    for (int i = 0; i < vertices; i++) { // robimy to samo co w bellmanFord + dodatkowe przejście, jeśli w dodatkowym przejściu nastąpi zmiana to mamy ujemny cykl
+        x = -1;
+
+        for(int u = 0; u < vertices; u++){
+            for (Edge* e : adjList[u]) {
+                if(e->capacity > e->flow && dist[e->to] > dist[u] + e->repairCost){
+                    dist[e->to] = dist[u] + e->repairCost;
+                    parent[e->to] = e;
+                    x = e->to;
+                }
+            }
+        }
+    }
+
+    if (x == -1) return false;
+
+    for (int i = 0; i < vertices; i++) {
+        x = parent[x]->from;
+    } // cofamy się, x wskazuje na wierzchołek w cyklu ujemnym, cofamy się vertices razy dzięki czemu x znajduje się na cyklu
+
+    cycle.clear();
+
+    int cur = x;
+    do {
+        cycle.push_back(parent[cur]);
+        cur = parent[cur]->from; // rekonstruujemy cykl cofając się po parent
+    } while(cur != x);
+
+    std::reverse(cycle.begin(), cycle.end());
+
+    return true;
+}
+
+void ResidualNetwork::cancelNegativeCycles(){
+    std::vector<Edge*> cycle;
+
+    while (findNegativeCycle(cycle)) {
+        int flowToAdd = std::numeric_limits<int>::max();
+
+        for (Edge* e : cycle) {
+            flowToAdd = std::min(flowToAdd, e->capacity - e->flow); // maksymalny możliwy przyrost przepływu = min(pojemność - przepływ);
+        }
+
+        for (Edge* e : cycle) { 
+            e->flow += flowToAdd; // dodajemy przepływ do każdej krawędzi
+            e->reverse->flow -= flowToAdd; // zmniejszamy przepływ na krawędziach odwrotnych
+        }
+    }
+}
+
 std::pair<int, int> ResidualNetwork::minCostMaxFlow(int source, int sink) {
     int maxFlow = 0; // makszymalny przepływ na zerto
     int minrepairCost = 0; // minimalny koszt na zero
     std::vector<Edge*> parent(vertices); // poprzednicy
 
+    cancelNegativeCycles();
+
     while (bellmanFord(source, sink, parent)) { // idziemy od ujścia do źródła / przechodzenie wstecz
         int pathFlow = std::numeric_limits<int>::max();
 
+        
+
         for (int v = sink; v != source; v = parent[v]->from) {
             pathFlow = std::min(pathFlow, parent[v]->capacity - parent[v]->flow); // to samo co w maxFlow bierzemy najmniejszą wartość ze ścieżki
+            //std::cout << "utknąłem" << std::endl;
         }
 
         for (int v = sink; v != source; v = parent[v]->from) { // aktualizacja przepływu wzdłuż ścieżki powiększającej, odejmowanie athFlow
