30ee0b940f0e26990fc9359569165ce8bb809a5c
lukix013 <70850792+lukix013@users.noreply.github.com>
2025-06-22 14:42:36 +0200

    Update residual.h

--- residual.h
+++ residual.h
@@ -10,12 +10,13 @@
 #include "prz_wiel.h"
 
 struct Edge {
-    int from, to; //skad, dokad
-    int capacity, flow;
-    int repairCost;  // koszt naprawy tej krawędzi
-    Edge* reverse;
+    int from;          //wierzchołek początkowy krawędzi
+    int to;            // wierzchołek końcowy krawędzi
+    int capacity;      // przepustowość krawędzi
+    int flow;          //aktualny przepływ przez krawędź
+    int repairCost;    // koszt naprawy (używany jako koszt przepływu)
+    Edge* reverse;     //wskaźnik do krawędzi rewersyjnej w grafie
 
-    // Konstruktor z parametrem kosztu naprawy
     Edge(int from, int to, int capacity, int repairCost = 0)
         : from(from), to(to), capacity(capacity), flow(0), repairCost(repairCost), reverse(nullptr) {}
 };
@@ -24,37 +25,25 @@ class ResidualNetwork {
 public:
     ResidualNetwork(int vertices);
 
-    // Stara wersja addEdge bez kosztu (just in case)
     void addEdge(int from, int to, int capacity);
-
-    // Nowa wersja addEdge z kosztem naprawy
     void addEdge(int from, int to, int capacity, int repairCost);
-
-    // podstawowa Werjsa
     int maxFlow(int source, int sink);
-
-    // Funkcja minimalnego kosztu maksymalnego przepływu
     std::pair<int, int> minCostMaxFlow(int source, int sink);
 
-    std::vector<std::vector<Edge*>> adjList;
-
+    std::vector<std::vector<Edge*>> adjList; // Lista sąsiedztwa przechowująca wskaźniki do krawędzi.
     void setCoordinates(int vertex, Point p);
+
     void printCoordinates();
 
-    //wierzcholek -> int x, int y
-    std::unordered_map<int, Point> coordinates;
+    std::unordered_map<int, Point> coordinates; //Mapa wierzchołków do ich współrzędnych.
 
 private:
-    int vertices;
-
-    
-    
-
+    int vertices; //Liczba wierzchołków w grafie.
 
     bool bfs(int source, int sink, std::vector<Edge*>& parent);
-
-    // Bellman-Ford do znajdowania najtańszej ścieżki powiększającej
     bool bellmanFord(int source, int sink, std::vector<Edge*>& parent);
+    bool findNegativeCycle(std::vector<Edge*> &cycle);
+    void cancelNegativeCycles();
 };
 
 #endif // RESIDUAL_H
